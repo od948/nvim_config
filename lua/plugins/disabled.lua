fe82a9f247e8dd4a24c485f0202afa81e7fbb4d3
Oliver <oliver.dunn@accelo.com>
2023-04-03 22:28:33 +1000

    inital

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  -- disable bufferline
+  { "akinsho/bufferline.nvim", enabled = false },
+  -- disable illuminate
+  { "RRethy/vim-illuminate", enabled = false },
+  -- disable notify
+  { "rcarriga/nvim-notify", enabled = false },
+  -- disable noice
+  { "folke/noice.nvim", enabled = false },
+}
