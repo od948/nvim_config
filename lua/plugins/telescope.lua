dcff40238d3e7eaf8a1f551a8e97450adb5f95f5
od948 <oged97@gmail.com>
2024-08-17 15:19:15 +1000

    update telescope version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	'nvim-telescope/telescope.nvim', 
-	tag = '0.1.2',
+	tag = '0.1.8',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	keys = {
 		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
