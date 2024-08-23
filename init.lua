a1576834f710ea810f408e93255caa89d97e7055
od948 <oged97@gmail.com>
2024-08-23 21:51:51 +1000

    update trouble, update general packages, change tabstop to 4

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.g.mapleader = " " -- set leader to space
-vim.opt.tabstop = 8
+vim.opt.tabstop = 4
 vim.opt.expandtab = false;
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
--- lua/plugins/trouble_nvim.lua
+++ lua/plugins/trouble_nvim.lua
@@ -1,10 +1,37 @@
 return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	keys = {
-		{ "<leader>xx", "<cmd>TroubleToggle<cr>", desc="Toggle Trouble" },
-		{ "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc="Touble Workspace" },
-		{ "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc="Trouble Quickfix" },
-		{ "<leader>gR", "<cmd>TroubleToggle lsp_references<cr>", desc="Trouble get_reference" },
-	},
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
 }
